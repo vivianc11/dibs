9715d1bccd47b473c3169c3a352530421e31e81a
Vivian Chen <vivian.chen1193@gmail.com>
2022-12-21 14:08:01 -0700

    added deleting post in post route

--- controllers/api/postRoute.js
+++ controllers/api/postRoute.js
@@ -55,5 +55,25 @@ router.get('/:id', async (req, res) => {
     }
 });
 
+// DELETE POST
+router.delete('/:id', async (req, res) => {
+    try {
+      // console.log("Test");
+      const postData = await Post.destroy({
+        where: {
+          id: req.params.id
+        },
+      });
+      // console.log("Testing");
+      if (!postData) {
+        res.status(404).json({ message: 'No post found with this id!' });
+        return;
+      }
+  
+      res.status(200).json(postData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+});
 
 module.exports = router;
