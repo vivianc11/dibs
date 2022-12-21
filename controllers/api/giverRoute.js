1b2cf8442e7094a2af5a53967c7fe55be0b2366f
Vivian Chen <vivian.chen1193@gmail.com>
2022-12-21 13:52:03 -0700

    adding route for logging in and validating password through bcrypt

--- controllers/api/giverRoute.js
+++ controllers/api/giverRoute.js
@@ -42,3 +42,44 @@ router.get('/', async (req, res) => {
         res.status(500).json(err)
     }
 })
+
+// Logging in
+router.post('/login', async (req, res) => {
+    try {
+        const userData = await Giver.findOne({ where: { username: req.body.username } });
+    
+        if (!userData) {
+          res
+            .status(400)
+            .json({ message: 'Incorrect username or password, please try again' });
+          return;
+        }
+        
+        // Since we used bcrypt npm to hash the password, we need to use it to compare the hashed password with the user's actual password
+        const validPassword = await bcrypt.compare(req.body.password, userData.password);
+    
+        if (!validPassword) {
+          res
+            .status(400)
+            .json({ message: 'Incorrect username or password, please try again' });
+          return;
+        }
+        // Testing to make sure we are getting correct information
+        // console.log("this is the user that logged in")
+        // console.log(userData)
+        req.session.save(() => {
+          req.session.username = userData.username;
+          req.session.user_id = userData.id;
+          req.session.logged_in = true;
+          res.json({ user: userData, message: 'You are now logged in!' });
+        });
+        
+        // console.log("This is the user id from session")
+        // console.log(req.session.user_id)
+      } catch (err) {
+        res.status(400).json(err);
+      }
+  });
+
+
+module.exports = router;
