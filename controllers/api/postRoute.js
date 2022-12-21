be0e44b617fdcb074d9122ffe61e37ebdcbf801b
Vivian Chen <vivian.chen1193@gmail.com>
2022-12-21 14:06:34 -0700

    adding finding all posts in post route

--- controllers/api/postRoute.js
+++ controllers/api/postRoute.js
@@ -19,3 +19,20 @@ router.post('/', async (req, res) => {
       res.status(400).json(err);
   }
 });
+
+// Find all posts
+router.get('/', async (req, res) => {
+    console.log('Hello');
+    try {
+        const findAll = await Post.findAll()
+        // console.log("Test");
+        // console.log(findAll);
+        res.json(findAll);
+    } catch (err) {
+        console.log(err)
+        res.status(500).json(err)
+    }
+  })
+
+
+module.exports = router;
