5fe60905c903d5930675c4594a41bcd5f45e4300
Vivian Chen <vivian.chen1193@gmail.com>
2022-12-21 14:02:34 -0700

    added deleting item in item route

--- controllers/api/itemRoute.js
+++ controllers/api/itemRoute.js
@@ -57,3 +57,25 @@ router.post('/', async (req, res) => {
         res.status(400).json(err);
     }
   });
+
+// DELETE ITEM
+router.delete('/:id', async (req, res) => {
+    try {
+      // console.log("Test");
+      const itemData = await Item.destroy({
+        where: {
+          id: req.params.id
+        },
+      });
+     
+      // console.log("Testing");
+      if (!itemData) {
+        res.status(404).json({ message: 'No item found with this id!' });
+        return;
+      }
+  
+      res.status(200).json(itemData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
