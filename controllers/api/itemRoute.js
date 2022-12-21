91f6b172341823e6434af82b800dab6a1fd228d2
Vivian Chen <vivian.chen1193@gmail.com>
2022-12-21 13:57:25 -0700

    added finding one item in item route

--- controllers/api/itemRoute.js
+++ controllers/api/itemRoute.js
@@ -14,3 +14,24 @@ router.get('/', async (req, res) => {
       res.status(500).json(err)
   }
 })
+
+// FIND ONE ITEM
+router.get('/:id', async (req, res) => {
+    try {
+      // console.log("Test");
+      const itemData = await Item.findOne({
+        where: {
+          id: req.params.id
+        },
+      });
+      // console.log("Testing");
+      if (!itemData) {
+        res.status(404).json({ message: 'No item found with this id!' });
+        return;
+      }
+  
+      res.status(200).json(itemData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
